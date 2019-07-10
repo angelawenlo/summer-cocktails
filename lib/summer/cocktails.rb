993942f5c31a352052b9b4327f1d7dbd23048c11
Angela Lo <angelawenlo@gmail.com>
2019-07-10 12:07:06 -0400

    initial commit

---
+++ lib/summer/cocktails.rb
@@ -0,0 +1,8 @@
+require "summer/cocktails/version"
+
+module Summer
+  module Cocktails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/summer/cocktails/version.rb
@@ -0,0 +1,5 @@
+module Summer
+  module Cocktails
+    VERSION = "0.1.0"
+  end
+end
